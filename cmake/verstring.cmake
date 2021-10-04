f887ee2927fdfe5fac4be6ec3c6527725534d093
Michal Schulz <michal.schulz@gmx.de>
2021-10-04 09:19:01 +0200

    Set MPLv2 license on that project

--- cmake/verstring.cmake
+++ cmake/verstring.cmake
@@ -1,4 +1,12 @@
+# Copyright © 2020 Michal Schulz <michal.schulz@gmx.de>
+# https://github.com/michalsc
+#
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # Build version string in Amiga-like manner: $VER: project_name version (build_date) git:git_hash"
+
 function(get_verstring OUTPUT)
     find_package(Git)
     string(TIMESTAMP PROJECT_DATE "(%d.%m.%Y)")
--- devicetree.resource/src/devicetree.h
+++ devicetree.resource/src/devicetree.h
@@ -1,6 +1,15 @@
 #ifndef __DEVICETREE_H
 #define __DEVICETREE_H
 
+/*
+    Copyright © 2021 Michal Schulz <michal.schulz@gmx.de>
+    https://github.com/michalsc
+
+    This Source Code Form is subject to the terms of the
+    Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+    with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #include <exec/types.h>
 #include <exec/nodes.h>
 #include <exec/lists.h>
