24cd3f16144f935aa4cb0856f4d0b6800d63dbc4
Michal Schulz <michal.schulz@gmx.de>
2021-10-19 14:24:33 +0200

    68040 support library added to the system

---
+++ 68040.library/src/base.h
@@ -0,0 +1,16 @@
+#ifndef _BASE_H
+#define _BASE_H
+
+#include <exec/libraries.h>
+
+#define MANUFACTURER_ID     0x6d73
+#define PRODUCT_ID          0x01
+#define SERIAL_NUMBER       0x4c32 
+#define LIB_VERSION         1
+#define LIB_REVISION        0
+#define LIB_PRIORITY        80
+
+#define LIB_POSSIZE         (sizeof(struct Library))
+#define LIB_NEGSIZE         (4*6)
+
+#endif /* _BASE_H */
