9a641b9e6e2cfbfe917707be8bcd73301ec51bf9
Michal Schulz <michal.schulz@gmx.de>
2021-12-13 21:56:34 +0100

    bumped up version number of sdcard

--- sdcard.device/src/sdcard.h
+++ sdcard.device/src/sdcard.h
@@ -120,7 +120,7 @@ struct SDCardUnit {
 void UnitTask();
 
 #define SDCARD_VERSION  1
-#define SDCARD_REVISION 0
+#define SDCARD_REVISION 1
 #define SDCARD_PRIORITY 20
 
 #define UNIT_TASK_PRIORITY  10
