3f8ede7b584918ec2b23ae7f2376950bac9b0ae1
Michal Schulz <michal.schulz@gmx.de>
2023-04-03 16:39:31 +0200

    a bit of debug added to send_i2c and receive_i2c

--- i2c.library/src/lib_functions/receive_i2c.c
+++ i2c.library/src/lib_functions/receive_i2c.c
@@ -6,7 +6,10 @@
 #include <proto/exec.h>
 #include <common/endian.h>
 #include <common/bcm_i2c.h>
+#include <common/debug.h>
 #include <i2c_private.h>
+
+#define D(x) /* x */
 
 #define RESULT(isSuccess, ubIoError, ubAllocError) ((ubAllocError << 16) | (ubIoError << 8) | (isSuccess))
 
@@ -17,12 +20,16 @@ ULONG ReceiveI2C(
 	REGARG(struct I2C_Base *i2cBase, "a6")
 )
 {
+	D(bug("ReceiveI2C(%02x, %d, %08x)\n", ubAddress, uwDataSize, pData));
+
 	// TODO: Semaphore shared with SendI2C
 	// bcm expects read/write bit to be omitted from address
 	ubAddress >>= 1;
 
 	volatile tI2cRegs * const pI2c = (volatile tI2cRegs *)i2cBase->I2cHwRegs;
 	UBYTE isSuccess = 1, ubIoError = I2C_OK, ubAllocError = I2C_OK;
+
+	D(bug("  S: %08x\n", rd32le(&pI2c->S)));
 
 	wr32le(&pI2c->A, ubAddress);
 	wr32le(&pI2c->C, I2C_C_CLEAR_FIFO_ONE_SHOT);
@@ -34,20 +41,26 @@ ULONG ReceiveI2C(
 	while(!(rd32le(&pI2c->S) & I2C_S_DONE)) {
 		while(uwDataSize && (rd32le(&pI2c->S) & I2C_S_RXD)) {
 			*(pData++) = rd32le(&pI2c->FIFO) & 0xFF;
+			D(bug("  Received %02x from FIFO\n", pData[-1]));
 			uwBytesRead++;
 			--uwDataSize;
 		}
 	}
 
+	D(bug("  Read completed. Draining FIFO\n"));
+
 	// Transfer done, now read all pending stuff in fifo
 	while(uwDataSize && (rd32le(&pI2c->S) & I2C_S_RXD)) {
 		*(pData++) = rd32le(&pI2c->FIFO) & 0xFF;
+		D(bug("  Received %02x from FIFO\n", pData[-1]));
 		uwBytesRead++;
 		--uwDataSize;
 	}
 
 	ULONG ulStatus = rd32le(&pI2c->S);
 	wr32le(&pI2c->S, I2C_S_DONE);
+
+	D(bug("  S: %08x\n", ulStatus));
 
 	if((ulStatus & (I2C_S_ERR | I2C_S_CLKT)) || uwDataSize) {
 		isSuccess = 0;
@@ -57,5 +70,8 @@ ULONG ReceiveI2C(
 
 	++i2cBase->RecvCalls;
 	i2cBase->RecvBytes += uwBytesRead;
+
+	D(bug("ReceiveI2C returns %08x\n", RESULT(isSuccess, ubIoError, ubAllocError)));
+
 	return RESULT(isSuccess, ubIoError, ubAllocError);
 }
--- i2c.library/src/lib_functions/send_i2c.c
+++ i2c.library/src/lib_functions/send_i2c.c
@@ -6,7 +6,10 @@
 #include <proto/exec.h>
 #include <common/endian.h>
 #include <common/bcm_i2c.h>
+#include <common/debug.h>
 #include <i2c_private.h>
+
+#define D(x) /* x */
 
 #define RESULT(isSuccess, ubIoError, ubAllocError) ((ubAllocError << 16) | (ubIoError << 8) | (isSuccess))
 
@@ -17,6 +20,8 @@ ULONG SendI2C(
 	REGARG(struct I2C_Base *i2cBase, "a6")
 )
 {
+	D(bug("SendI2C(%02x, %d, %08x)\n", ubAddress, uwDataSize, pData));
+
 	// TODO: Semaphore shared with ReceiveI2C
 
 	// bcm expects read/write bit to be omitted from address
@@ -24,6 +29,8 @@ ULONG SendI2C(
 
 	volatile tI2cRegs * const pI2c = (volatile tI2cRegs *)i2cBase->I2cHwRegs;
 	UBYTE isSuccess = 1, ubIoError = I2C_OK, ubAllocError = I2C_OK;
+
+	D(bug("  S: %08x\n", rd32le(&pI2c->S)));
 
 	wr32le(&pI2c->A, ubAddress);
 	wr32le(&pI2c->C, I2C_C_CLEAR_FIFO_ONE_SHOT);
@@ -34,6 +41,7 @@ ULONG SendI2C(
 	UWORD uwBytesCopied = 0;
 	while(!(rd32le(&pI2c->S) & I2C_S_DONE)) {
 		while(uwDataSize && (rd32le(&pI2c->S) & I2C_S_TXW)) {
+			D(bug("  Writing %02x to FIFO\n", *pData));
 			wr32le(&pI2c->FIFO, *(pData++));
 			uwBytesCopied++;
 			--uwDataSize;
@@ -43,6 +51,8 @@ ULONG SendI2C(
 	ULONG ulStatus = rd32le(&pI2c->S);
 	wr32le(&pI2c->S, I2C_S_DONE);
 
+	D(bug("  S: %08x\n", ulStatus));
+
 	if((ulStatus & (I2C_S_ERR | I2C_S_CLKT)) || uwDataSize) {
 		isSuccess = 0;
 		++i2cBase->Unheard;
@@ -51,5 +61,8 @@ ULONG SendI2C(
 
 	++i2cBase->SendCalls;
 	i2cBase->SendBytes += uwBytesCopied;
+
+	D(bug("SendI2C returns %08x\n", RESULT(isSuccess, ubIoError, ubAllocError)));
+
 	return RESULT(isSuccess, ubIoError, ubAllocError);
 }
